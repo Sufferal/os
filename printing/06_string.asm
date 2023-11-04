ea6d84e5ce37cef984109aee0128d55ea296dce0
destiny <botnari_ciprian@mail.ru>
2023-11-04 21:23:41 +0200

    fix: remove jmp; add clean.sh

--- printing/06_string.asm
+++ printing/06_string.asm
@@ -1,18 +1,15 @@
-org 0x7c00 ; Set origin to 0x7c00
-
-jmp start ; Jump to start of program
-
-section .data
-  msg db "Welcome to Assembly Club", 0  ; db = define byte, 0 = null terminator
-  msg_len equ $-msg                     ; $ = current address, $-msg = length of message
+org 0x7c00 ; Load program at address 0x7c00
 
 start:
-  mov SP, 0x7c00  ; Set stack pointer to 0x7c00
-  mov AX, 1300h   ; Function 13h, Write String
-  mov AL, 0       ; Subservice 0
+  mov AH, 13h     ; Function 13h, Write String
+  mov AL, 0       ; Do not update cursor
   mov BL, 14      ; Attribute/Color of text (14 = yellow)
   mov DH, 1       ; Row position where string is to be written
   mov DL, 25      ; Column position where string is to be written	
   mov CX, msg_len ; Length of message
   mov BP, msg     ; Pointer to message
   int 10h         ; Call BIOS video interrupt
+
+section .data
+  msg db "Hello to Assembly Club", 0  ; db = define byte, 0 = null terminator
+  msg_len equ $-msg        
--- printing/07_string_cursor.asm
+++ printing/07_string_cursor.asm
@@ -1,18 +1,15 @@
-org 0x7c00 ; Set origin to 0x7c00
-
-jmp start ; Jump to start of program
-
-section .data
-  msg db "Welcome to Assembly Club"     ; db = define byte
-  msg_len equ $-msg                     ; $ = current address, $-msg = length of message
+org 0x7c00 ; Load program at address 0x7c00
 
 start:
-  mov SP, 0x7c00  ; Set stack pointer to 0x7c00
-  mov AX, 1300h   ; Function 13h, Write String
-  mov AL, 1       ; Subservice 0
+  mov AH, 13h     ; Function 13h, Write String
+  mov AL, 1       ; Update cursor after writing
   mov BL, 14      ; Attribute/Color of text (14 = yellow)
   mov DH, 1       ; Row position where string is to be written
   mov DL, 25      ; Column position where string is to be written	
   mov CX, msg_len ; Length of message
   mov BP, msg     ; Pointer to message
   int 10h         ; Call BIOS video interrupt
+
+section .data
+  msg db "Welcome to Assembly Club"     ; db = define byte
+  msg_len equ $-msg                     ; $ = current address, $-msg = length of message
